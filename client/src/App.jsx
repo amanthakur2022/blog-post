1f7dbe281232b37304810f2ce9d126a905623b4f
Aman Thakur <aman.katal@lpinfotech.com>
2024-09-02 13:43:46 +0530

    installed material tailwindcss framework

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from "@material-tailwind/react";
 
 
 export default function App() {
+
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<Button>Button</Button>
 		</>
 	);
 }
